8d2a3ca7c4e3b8bcc473454b375da9f1fd357fb5
Mehakpreet Singh <mehakbrar811@gmail.com>
2023-10-22 12:42:29 +0530

    Typewriter added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,54 +12,62 @@ import Bounce from '../Bounce/Bounce';
 import Timeline from '../Timeline/Timeline';
 import Footer from '../Footer/Footer';
 // import Bounce from '../Bounce';
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Home = () => {
-  return (
-    <>
-        <div className='Scroll-container'>
-            <div className='flex-container'>
-                {/* <img className='logo-img' src={require('../../Sources/cicca.png')}/> */}
-                {/* <h1>Cicca Defi</h1>
+
+    const [text] = useTypewriter({
+        words : ['Developer' , 'Designer' , 'Photographer'],
+        loop : 1
+      })
+    return (
+        <>
+            <div className='Scroll-container'>
+                <div className='flex-container'>
+                    <span>{text}</span>
+                    <Cursor/>
+                    {/* <img className='logo-img' src={require('../../Sources/cicca.png')}/> */}
+                    {/* <h1>Cicca Defi</h1>
                 <h2>The Future of Gaming</h2> */}
-                <h3>Defi Web3Game, staking dApp, #Nft Minting AI chat, </h3> <h3> #Metaverse & more! Swap, stake, play-to-earn, & dive into new realms.</h3>
-                
+                    <h3>Defi Web3Game, staking dApp, #Nft Minting AI chat, </h3> <h3> #Metaverse & more! Swap, stake, play-to-earn, & dive into new realms.</h3>
+
                     <Bounce />
-                <div className='button-bar'>
-                    <button className='button1'>WHITEPAPER</button>
-                    <button className='button2'>LAUNCHPAD</button>
-                </div>
-                {/* <div className='powered_h1'>
+                    <div className='button-bar'>
+                        <button className='button1'>WHITEPAPER</button>
+                        <button className='button2'>LAUNCHPAD</button>
+                    </div>
+                    {/* <div className='powered_h1'>
                     POWERED BY
                 </div>
                 <div className='powered-bar'>
                     <img src="https://artyfact.game/wa-data/public/site/themes/artyfact/logo/unreal4.png" width="85px" class="img-fluid"/>
                     <img src="https://artyfact.game/wa-data/public/site/themes/artyfact/logo/binance.png" width="85px" class="img-fluid"/>
                 </div> */}
-                {/* <div className='powered_h1'>
+                    {/* <div className='powered_h1'>
                     SECURITY AUDIT BY
                 </div>
                 <div className='powered-bar'>
                     <img data-cfsrc="/wa-data/public/site/themes/artyfact/logo/certik.png" class="img-fluid" src="https://artyfact.game/wa-data/public/site/themes/artyfact/logo/certik.png"/>
                 </div> */}
-                {/* <div className='powered_h1'>
+                    {/* <div className='powered_h1'>
                     TEST CICCA
                 </div> */}
-                <div className='powered-bar'>
-                    <button className='btn button1'>CICCA DEMO</button>
+                    <div className='powered-bar'>
+                        <button className='btn button1'>CICCA DEMO</button>
+                    </div>
                 </div>
+                {/* <TokenBlock/> */}
+                <About />
+                {/* <Bounce /> */}
+                {/* <Feature/> */}
+                {/* <Games/> */}
+                <NFT />
+                <Timeline />
+                {/* <RoadMap/> */}
+                <Footer />
             </div>
-            {/* <TokenBlock/> */}
-            <About/>
-            {/* <Bounce /> */}
-            {/* <Feature/> */}
-            {/* <Games/> */}
-            <NFT/>
-            <Timeline />
-            {/* <RoadMap/> */}
-            <Footer/>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Home
